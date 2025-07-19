e28385743bec8cfd9f5d905597779dc5e87e250a
yrighting <yrighting@gmail.com>
2025-07-19 15:04:38 +0900

    Made Login Page

--- app/src/main/java/com/example/sixsense/SignIn.kt
+++ app/src/main/java/com/example/sixsense/SignIn.kt
@@ -1,11 +1,30 @@
 package com.example.sixsense
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val longinButton = findViewById<Button>(R.id.loginbutton)
+
+        longinButton.setOnClickListener{
+            val id = findViewById<EditText>(R.id.idEditText).text.toString()
+            val pw = findViewById<EditText>(R.id.pwEditTextPassword).text.toString()
+
+            if(id.isNotEmpty()){
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this, "아이디와 비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
